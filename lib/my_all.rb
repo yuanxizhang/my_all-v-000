9485bd835ab7cb922eaf9a941267ee1d5c5f678f
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-03 02:14:23 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,13 +2,12 @@ require 'pry'
 
 def my_all?(collection)
   i = 0 
-  block_return_values = []
+  return_values = []
   while i < collection.length 
-    block_return_values << yield collection[i]
+    return_values << yield (collection[i])
     i += 1 
-  end 
   
-  if block_return_values.include?(false) 
+  if return_values.include?(false) 
     false
   else
     true 
